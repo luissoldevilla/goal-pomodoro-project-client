e30a7f2d48200f306cfd90149b90e928124e4625
luissoldevilla <luis.angelsoto@hotmail.com>
2018-06-28 15:42:53 -0400

    Change url to production

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://sleepy-tor-47880.herokuapp.com>',
+  production: 'https://sleepy-tor-47880.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
